6f8609065055300cafcdc7aaf43bf9e10423d1f5
tianzhaoming <tianchaoming@codemao.cn>
2025-12-15 16:08:44 +0800

    继续

--- device/operation/TaskOperation.py
+++ device/operation/TaskOperation.py
@@ -3,6 +3,12 @@ from typing import Callable, Any
 from device.config.TaskRandomConfig import TaskRandomConfig
 from logevent.Log import Log
 
+COLOR_RED = '\033[91m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+BLUE = '\033[94m'
+COLOR_RESET = '\033[0m'  # 重置颜色
+
 
 class TaskOperation(TaskRandomConfig):
     def __init__(self):
@@ -13,4 +19,4 @@ class TaskOperation(TaskRandomConfig):
         Log.d("主任务次数", str(times))
         for i in range(times):
             callback()
-            Log.d("主任务次数剩", str(times - i - 1))
+            Log.d(f"{COLOR_RED}主任务次数剩:{str(times - i - 1)}{COLOR_RESET}")
