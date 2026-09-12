ffb65730e34ed56859a72b0add1a97dc52e9d4ac
9527 <1146699396@qq.com>
2026-09-12 13:16:17 +0800

    继续

--- device/DeviceInfo.py
+++ device/DeviceInfo.py
@@ -5,8 +5,8 @@ class DeviceInfo:
 
 class Mi15(DeviceInfo):
     def __init__(self):
-        super().__init__("192.168.0.107")
-        # super().__init__("23a6524e")
+        # super().__init__("192.168.0.102")
+        super().__init__("23a6524e")
 
 class HwP40(DeviceInfo):
     def __init__(self):
